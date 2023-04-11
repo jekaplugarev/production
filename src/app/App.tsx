9087ecc4ef93304ed7016548341ea56782eab8ba
jekaplugarev <jeka_plugarev@mail.ru>
2023-04-11 21:17:14 +0200

    Новые компоненты

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,42 @@
-import {FC} from 'react'
+import {FC, Suspense} from 'react'
 import "./styles/index.scss"
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
 import {AppRouter} from "app/providers/router";
 import {Navbar} from "widgets/Navbar";
 import {Sidebar} from "widgets/Sidebar";
+import {useTranslation} from "react-i18next";
+
+const Component = () => {
+    const {t, i18n} = useTranslation()
+
+    const toggle = () => {
+        i18n.changeLanguage(i18n.language === "ru" ? "en" : "ru")
+    }
+
+    return (
+        <div>
+            <button onClick={toggle}>
+                {t("Перевод")}
+            </button>
+            {t("Текстовый пример")}
+        </div>
+    )
+}
 
 const App: FC = () => {
     const {theme} = useTheme()
 
     return (
         <div className={classNames("app", {},[theme])}>
-            <Navbar/>
-            <div className="content-page">
-                <Sidebar/>
-                <AppRouter/>
-            </div>
+            <Suspense fallback="">
+                <Component/>
+                <Navbar/>
+                <div className="content-page">
+                    <Sidebar/>
+                    <AppRouter/>
+                 </div>
+            </Suspense>
         </div>
     )
 }
--- src/widgets/Sidebar/ui/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar.tsx
@@ -2,6 +2,7 @@ import {FC, useState} from 'react'
 import {classNames} from "shared/lib/classNames/classNames";
 import cls from "./Sidebar.module.scss"
 import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
+import {LangSwitcher} from "shared/ui/LangSwitcher";
 
 interface SidebarProps {
     className?: string;
@@ -19,6 +20,7 @@ export const Sidebar: FC<SidebarProps> = ({className}) => {
             </button>
             <div className={cls.switchers}>
                 <ThemeSwitcher/>
+                <LangSwitcher className={cls.lang}/>
             </div>
         </div>
     )
