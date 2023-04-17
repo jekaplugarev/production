af2855f3c384f55d38b5e5045af6f543d7ff498e
jekaplugarev <jeka_plugarev@mail.ru>
2023-04-17 21:27:41 +0200

    tests config

--- src/widgets/Sidebar/ui/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar.tsx
@@ -2,6 +2,7 @@ import { FC, useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import { LangSwitcher } from 'shared/ui/LangSwitcher';
+import { Button } from 'shared/ui/Button';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -14,10 +15,16 @@ export const Sidebar: FC<SidebarProps> = ({ className }) => {
     const onToggle = () => setCollapsed((prev) => !prev);
 
     return (
-        <div className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}>
-            <button type="button" onClick={onToggle}>
+        <div
+            data-testid="sidebar"
+            className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}
+        >
+            <Button
+                onClick={onToggle}
+                data-testid="sidebar-toggle"
+            >
                 Toggle
-            </button>
+            </Button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher className={cls.lang} />
