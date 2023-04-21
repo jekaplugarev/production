e24192eef37578e59c75543f8318afd6b0e27eea
jekaplugarev <jeka_plugarev@mail.ru>
2023-04-21 15:09:19 +0200

    fix

--- webpack.config.ts
+++ webpack.config.ts
@@ -5,7 +5,7 @@ import { BuildEnv, BuildPaths } from './config/build/types/config';
 
 export default (env: BuildEnv) => {
     const paths: BuildPaths = {
-        entry: path.resolve(__dirname, 'src', 'index.ts'),
+        entry: path.resolve(__dirname, 'src', 'index.tsx'),
         build: path.resolve(__dirname, 'build'),
         html: path.resolve(__dirname, 'public', 'index.html'),
         src: path.resolve(__dirname, 'src'),
@@ -13,6 +13,7 @@ export default (env: BuildEnv) => {
 
     const mode = env.mode || 'development';
     const PORT = env.port || 3000;
+
     const isDev = mode === 'development';
 
     const config: webpack.Configuration = buildWebpackConfig({
