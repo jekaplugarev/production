0d2d32c38e500778c52360bc9f7b72325a1a96b9
jekaplugarev <jeka_plugarev@mail.ru>
2023-04-24 15:00:52 +0200

    modal and portal

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,28 +1,9 @@
 import { FC, Suspense } from 'react';
-import './styles/index.scss';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import { useTranslation } from 'react-i18next';
-
-const Component = () => {
-    const { t, i18n } = useTranslation();
-
-    const toggle = () => {
-        i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
-    };
-
-    return (
-        <div>
-            <button type="button" onClick={toggle}>
-                {t('Перевод')}
-            </button>
-            {t('Текстовый пример')}
-        </div>
-    );
-};
 
 const App: FC = () => {
     const { theme } = useTheme();
@@ -30,7 +11,6 @@ const App: FC = () => {
     return (
         <div className={classNames('app', {}, [theme])}>
             <Suspense fallback="">
-                <Component />
                 <Navbar />
                 <div className="content-page">
                     <Sidebar />
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,15 +1,40 @@
-import { FC } from 'react';
+import { FC, useCallback, useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { Modal } from 'shared/ui/Modal';
+import { useTranslation } from 'react-i18next';
+import { Button } from 'shared/ui/Button';
+import { ButtonTheme } from 'shared/ui/Button/ui/Button';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
     className?: string;
 }
 
-export const Navbar: FC<NavbarProps> = ({ className }) => (
-    <div className={classNames(cls.Navbar, {}, [className])}>
-        <div className={cls.links} />
-    </div>
-);
+export const Navbar: FC<NavbarProps> = ({ className }) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const { t } = useTranslation();
+
+    const onToggleModal = useCallback(() => setIsOpen((prev) => !prev), []);
+
+    return (
+        <div className={classNames(cls.Navbar, {}, [className])}>
+            <Button
+                theme={ButtonTheme.CLEAR_INVERTED}
+                onClick={onToggleModal}
+                className={cls.links}
+            >
+                {t('Войти')}
+            </Button>
+            {/* eslint-disable-next-line i18next/no-literal-string */}
+            <Modal isOpen={isOpen} onClose={onToggleModal}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Accusamus accusantium atque blanditiis culpa esse
+                exercitationem, harum inventore minima
+                natus nisi odit officiis quae quisquam repellat tempore temporibus
+                totam velit vero.
+            </Modal>
+        </div>
+    );
+};
 
 export default Navbar;
