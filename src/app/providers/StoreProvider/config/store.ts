b47c1667b547eefa9f218287bd6399d5d3ca0c66
jekaplugarev <jeka_plugarev@mail.ru>
2023-05-08 11:59:33 +0200

    api instance, url from environment, async thunk extra arguments

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -2,12 +2,14 @@ import { configureStore, ReducersMapObject } from '@reduxjs/toolkit';
 import { counterReducer } from 'entities/Counter';
 import { userReducer } from 'entities/User';
 import { createReducerManager } from 'app/providers/StoreProvider/config/reducerManager';
-import { useDispatch } from 'react-redux';
+import { $api } from 'shared/api/api';
+import { NavigateOptions, To } from 'react-router-dom';
 import { StateSchema } from './StateSchema';
 
 export function createReduxStore(
     initialState?: StateSchema,
     asyncReducers?: ReducersMapObject<StateSchema>,
+    navigate?: (to: To, options?: NavigateOptions) => void,
 ) {
     const rootReducers: ReducersMapObject<StateSchema> = {
         ...asyncReducers,
@@ -17,10 +19,18 @@ export function createReduxStore(
 
     const reducerManager = createReducerManager(rootReducers);
 
-    const store = configureStore<StateSchema>({
+    const store = configureStore({
         reducer: reducerManager.reduce,
         devTools: __IS_DEV__,
         preloadedState: initialState,
+        middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+            thunk: {
+                extraArgument: {
+                    api: $api,
+                    navigate,
+                },
+            },
+        }),
     });
 
     // @ts-ignore
--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
@@ -1,31 +1,33 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
 import { User } from 'entities/User';
 import { userActions } from 'entities/User/model/slice/userSlice';
 import { USER_LOCALSTORAGE_KEY } from 'shared/const/localStorage';
+import { ThunkConfig } from 'app/providers/StoreProvider';
 
 interface LoginByUsernameProps {
     username: string;
     password: string;
 }
 
-export const loginByUsername = createAsyncThunk<User, LoginByUsernameProps, {rejectValue: string}>(
-    'login/loginByUsername',
-    async (authData, thunkAPI) => {
-        try {
-            const response = await axios.post<User>('http://localhost:8000/login', authData);
+export const loginByUsername = createAsyncThunk<User,
+    LoginByUsernameProps,
+    ThunkConfig<string>>(
+        'login/loginByUsername',
+        async (authData, { dispatch, extra, rejectWithValue }) => {
+            try {
+                const response = await extra.api.post<User>('/login', authData);
 
-            if (!response.data) {
-                throw new Error();
+                if (!response.data) {
+                    throw new Error();
+                }
+
+                localStorage.setItem(USER_LOCALSTORAGE_KEY, JSON.stringify(response.data));
+                dispatch(userActions.setAuthData(response.data));
+
+                return response.data;
+            } catch (error) {
+                console.log(error);
+                return rejectWithValue('Error');
             }
-
-            localStorage.setItem(USER_LOCALSTORAGE_KEY, JSON.stringify(response.data));
-            thunkAPI.dispatch(userActions.setAuthData(response.data));
-
-            return response.data;
-        } catch (error) {
-            console.log(error);
-            return thunkAPI.rejectWithValue('Error');
-        }
-    },
-);
+        },
+    );
