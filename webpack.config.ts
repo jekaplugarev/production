0cd64a6a8e79299f725981d9a31571058768c334
jekaplugarev <jeka_plugarev@mail.ru>
2023-06-14 10:57:37 +0200

    refactoring

--- webpack.config.ts
+++ webpack.config.ts
@@ -9,6 +9,8 @@ export default (env: BuildEnv) => {
         build: path.resolve(__dirname, 'build'),
         html: path.resolve(__dirname, 'public', 'index.html'),
         src: path.resolve(__dirname, 'src'),
+        locales: path.resolve(__dirname, 'public', 'locales'), // i18n.ts
+        buildLocales: path.resolve(__dirname, 'build', 'locales'),
     };
 
     const mode = env.mode || 'development';
